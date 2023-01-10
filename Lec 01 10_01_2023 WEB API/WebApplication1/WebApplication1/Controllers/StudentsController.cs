837fd2c18308638376e190fca82b5512b13a8d5d
nc <31476486+nirc7@users.noreply.github.com>
2023-01-10 12:56:26 +0200

    api1

---
+++ Lec 01 10_01_2023 WEB API/WebApplication1/WebApplication1/Controllers/StudentsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        //api/students
+        public Student[] Get()
+        {
+            return DBStudentsMock.students.ToArray();
+        }
+
+        //api/students/2
+        public Student Get(int id)
+        {
+            Student s = DBStudentsMock.students.FirstOrDefault(stu=> stu.Id == id);
+            return s;
+        }
+    }
+}
