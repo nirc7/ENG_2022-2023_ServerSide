83e07e4e8d973bde731daea99dc26c304a02cedd
nc <31476486+nirc7@users.noreply.github.com>
2023-01-16 18:05:15 +0200

    webapi2

---
+++ Lec02 16_01_2023 WEB API2/WebApplication1/WebApplication1/Controllers/StudentsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        public List<Student> Get()
+        {
+            return StudentsDBMock.studnets;
+        }
+
+        public Student Get(int id)
+        {
+            return StudentsDBMock.studnets.FirstOrDefault(stu => stu.Id == id);
+        }
+
+        public int Post([FromBody] Student value)
+        {
+            StudentsDBMock.studnets.Add(value);
+            return value.Id;
+        }
+
+        public string Put(int id, [FromBody] Student value)
+        {
+            Student stu2Update = StudentsDBMock.studnets.FirstOrDefault(stu => stu.Id == id);
+            stu2Update.Name = value.Name;
+            stu2Update.Grade = value.Grade;
+            return "done:)";
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            Student stu2Del = StudentsDBMock.studnets.FirstOrDefault(stu=> stu.Id == id);
+            StudentsDBMock.studnets.Remove(stu2Del);
+            var v = new { Result= "delete successfully!" };
+            var j = Json(v);
+            return j;
+        }
+
+
+    }
+}
---
+++ Lec02 16_01_2023 WEB API2/WebApplication1/WebApplication1/Models/StudentsDBMock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public static class StudentsDBMock
+    {
+        public static List<Student> studnets = new List<Student>() {
+            new Student(){Id=1 ,Name="avi" , Grade=100 },
+            new Student(){Id=2 ,Name="dora" , Grade=97 },
+            new Student(){Id=3 ,Name="chalrie" , Grade=98 },
+            new Student(){Id=4 ,Name="benny" , Grade=99 },
+        };
+
+    }
+}
